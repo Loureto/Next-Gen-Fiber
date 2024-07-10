304289fd7ac8cf13fd09d969b61d34510a0dd132
joao <loureto.tads123@gmail.com>
2024-07-10 11:09:30 -0400

    fix: resolve imports and packages

--- src/shared/infra/http/axios-http-client.ts
+++ src/shared/infra/http/axios-http-client.ts
@@ -1,26 +1,26 @@
-import { HttpClient, HttpRequest, HttpResponse } from "@/core/data";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import { HttpClient, HttpRequest, HttpResponse } from '@/shared/data'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export class AxiosHttpClient implements HttpClient {
-    async request(data: HttpRequest): Promise<HttpResponse> {
-        let axiosResponse: AxiosResponse
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse
 
-        try {
-            axiosResponse = await axios.request({
-                url: data.url,
-                data: data.body,
-                method: data.method,
-                headers: data.headers
-            })    
-        } catch (error) {
-            const _error = error as AxiosError
+    try {
+      axiosResponse = await axios.request({
+        url: data.url,
+        data: data.body,
+        method: data.method,
+        headers: data.headers
+      })
+    } catch (error) {
+      const _error = error as AxiosError
 
-            axiosResponse = _error.response as AxiosResponse
-        }
-
-        return {
-            statusCode: axiosResponse.status,
-            body: axiosResponse.data
-        }
+      axiosResponse = _error.response as AxiosResponse
     }
+
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
+  }
 }
